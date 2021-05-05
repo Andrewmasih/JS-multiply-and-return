8597205ac37b32aea5217d14326a05d574c7e7bd
Andrew Masih <armasih@outlook.com>
2021-05-05 16:05:57 +0000

    added more notes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function multiply (a, b)  {
+/* Return, a keyword in Javascript, it returns a value from the function and stops further execution, just add the 'return' keyword bfore the expression you want to return,  */
 
+function multiply (a, b)  {
+  // the function takes 2 parameters a and b and returns the product
   return a*b;        
-  
+  /* call the multiply function with the argument 10 and 2. return like a blackbox, we put data in and we dont need to be aware of it, notice I used a 'return' statement in our 'multiply' function:*/
 } 
 
 multiply (10, 2);
+
+/* you can try this with any number  */
